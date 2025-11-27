3a50c69f6a10f7e987c7a44a5cc8935233b67867
tcc <trabalhotcc52@gmail.com>
2025-11-26 21:34:37 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,16 +1,5 @@
 const API_URL = "https://supermercado-2rnt.onrender.com";
 
-function addMessage(text, sender) {
-  const chat = document.getElementById("chat");
-
-  const div = document.createElement("div");
-  div.classList.add("message", sender);
-  div.innerText = text;
-
-  chat.appendChild(div);
-  chat.scrollTop = chat.scrollHeight;
-}
-
 async function sendMessage() {
   const input = document.getElementById("message");
   const msg = input.value.trim();
@@ -19,8 +8,14 @@ async function sendMessage() {
   addMessage(msg, "user");
   input.value = "";
 
-  const response = await fetch(API_URL + "?message=" + encodeURIComponent(msg));
-  const data = await response.json();
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: msg })
+  });
 
+  const data = await response.json();
   addMessage(data.reply, "bot");
 }
+
+
